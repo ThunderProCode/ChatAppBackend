01d6b34ab6e34b691dd79d2879653049c71fffa8
thunderprocode <hectora887ju@gmail.com>
2023-10-22 22:05:56 -0600

    Basic Server for testing done

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const http = require('http');
+// const path = require('path');
+// const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.listen(8080);
+
+const userSockets = [];
+function generateUniqueID() {
+    while (true) {
+        const randomNumber = Math.floor(100000 + Math.random() * 900000); // Generates a random number between 100000 and 999999
+        if (!userSockets[randomNumber]) {
+        return randomNumber;
+        }
+    }
+} 
+
+io.on('connection', (socket) => {
+    const userId = generateUniqueID();
+    socket.data.username = userId;
+    userSockets[userId] = socket;
+
+    socket.emit('username',socket.data.username);
+
+    socket.on('connection-username', (connectionUsername) => {
+        const targetSocket = userSockets[connectionUsername];
+        if(targetSocket) {
+            socket.emit('connected',targetSocket.data.username);
+            targetSocket.emit('connected',socket.data.username);
+        }
+    });
+
+    socket.on('chat message', (msg) => {
+        const targetSocket = userSockets[msg.to];
+        if(targetSocket) {
+            targetSocket.emit('chat message',msg.message);
+        }
+
+    });
+
+    socket.on('disconnect', () => {
+        delete userSockets[socket.data.username];
+    });
+
+})
+
